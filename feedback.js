f59e0be17213d0a21779cadaa7032cf5e001a399
RitaK7 <126379283+RitaK7@users.noreply.github.com>
2025-06-05 22:50:12 +0300

    Delete feedback.js
    I made mistake

--- feedback.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("feedbackForm");
-  const thanks = document.getElementById("thanksMessage");
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    form.style.display = "none";
-    thanks.style.display = "block";
-  });
-});
