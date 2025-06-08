67ab73790448ee581b4acb054e9baaddd215bf52
RitaK7 <126379283+RitaK7@users.noreply.github.com>
2025-06-09 00:07:55 +0300

    Secure Firebase config
    Secure Firebase config

--- firebase.js
+++ firebase.js
@@ -1,19 +1,9 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { firebaseConfig } from './config.js'; // Saugus importas
 
-// Saugūs kintamieji iš .env failo (naudojant Vite ar kitą bundlerį)
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-};
-
-// Inicializuojame Firebase
+// Inicializuojam Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
