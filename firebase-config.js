652a849943e31e3b37c3270df3130061a227c928
RitaK7 <126379283+RitaK7@users.noreply.github.com>
2025-06-16 00:48:48 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
+// -- firebase-config.js --
 const firebaseConfig = {
-  apiKey: "AIzaSyCb0TOXVrP1dQe16T6RNRvAg8lwu9QPnf4",
-  authDomain: "soulink-342bb.firebaseapp.com",
-  projectId: "soulink-342bb",
-  storageBucket: "soulink-342bb.appspot.com",
-  messagingSenderId: "541312933178",
-  appId: "1:541312933178:web:239cbbb60e68b183f48403",
-  measurementId: "G-7DQPK8J701"
+  apiKey:      "TAVO_REALUS_API_KEY",
+  authDomain:  "TAVO_PROJEKTAS.firebaseapp.com",
+  projectId:   "TAVO_PROJEKTAS",
+  storageBucket: "TAVO_PROJEKTAS.appspot.com",
+  messagingSenderId: "…",
+  appId: "…"
 };
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-export { db, auth };
+firebase.initializeApp(firebaseConfig);
