6a9d857d0ba6a37a4b9de9fcb919fa6b24a39c08
RitaK7 <126379283+RitaK7@users.noreply.github.com>
2025-06-16 01:19:01 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,15 @@
 // -- firebase-config.js --
 const firebaseConfig = {
-  apiKey:      "TAVO_REALUS_API_KEY",
-  authDomain:  "TAVO_PROJEKTAS.firebaseapp.com",
-  projectId:   "TAVO_PROJEKTAS",
-  storageBucket: "TAVO_PROJEKTAS.appspot.com",
-  messagingSenderId: "…",
-  appId: "…"
+  const firebaseConfig = {
+  apiKey: "AIzaSyCb0TOXVrP1dQe16T6RNrvAg8Iwu9QPnf4",
+  authDomain: "soulink-342bb.firebaseapp.com",
+  projectId: "soulink-342bb",
+  storageBucket: "soulink-342bb.appspot.com",
+  messagingSenderId: "541312933178",
+  appId: "1:541312933178:web:239cbbb60e68b183f48403"
+};
+
+// 2) Inicijuojam
+firebase.initializeApp(firebaseConfig);
 };
 firebase.initializeApp(firebaseConfig);
